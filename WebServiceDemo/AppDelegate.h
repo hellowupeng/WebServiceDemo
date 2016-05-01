5e5249d94c38c1ce8718d48103f60e130e56140a
Andy <hellowupeng@gmail.com>
2016-05-01 12:01:26 +0800

    Initial Commit

---
+++ WebServiceDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WebServiceDemo
+//
+//  Created by gaiaworks on 16/5/1.
+//  Copyright © 2016年 gaiaworks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
